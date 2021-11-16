63fee4ba5a48f3a24f3294daeabf195492b70a36
itagusu <88267436+itagusu@users.noreply.github.com>
2021-11-16 12:48:13 +0900

    タブメニュー

--- js/script.js
+++ js/script.js
@@ -32,9 +32,19 @@ $(function(){
 });
 
 $(function() {
-  $('.menu-trigger').on('click', function(event) {
+  $('#hm-icon a').on('click', function(event) {
     $(this).toggleClass('active');
     $('#sp-menu').fadeToggle();
     event.preventDefault();
   });
+});
+
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 });
